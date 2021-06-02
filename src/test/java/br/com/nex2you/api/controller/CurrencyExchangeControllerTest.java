4a918b06d2b4440dc8729d8548a0d46c20d2c469
Thiago Gregorio <thiagotgregorio@gmail.com>
2021-06-01 22:50:46 -0300

    Inclusao de testes

---
+++ src/test/java/br/com/nex2you/api/controller/CurrencyExchangeControllerTest.java
@@ -0,0 +1,14 @@
+package br.com.nex2you.api.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CurrencyExchangeControllerTest {
+
+	@Test
+	void testRetrieveExchangeValue() {
+		assertTrue(true);
+	}
+
+}
