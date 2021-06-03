bda8b08fd9b110a4011fbb42f1020fbd0d1f6085
Thiago Gregorio <thiagotgregorio@gmail.com>
2021-06-02 21:26:02 -0300

    Ajustes

---
+++ src/test/java/br/com/nex2you/api/CurrencyExchangeServiceApplicationTest.java
@@ -0,0 +1,8 @@
+package br.com.nex2you.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTest {
+
+}
--- src/test/java/br/com/nex2you/api/controller/CurrencyExchangeControllerTest.java
+++ src/test/java/br/com/nex2you/api/controller/CurrencyExchangeControllerTest.java
@@ -1,13 +1,25 @@
 package br.com.nex2you.api.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+@RunWith(MockitoJUnitRunner.Silent.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class CurrencyExchangeControllerTest {
 
+	@Autowired
+	protected MockMvc mockMvc;
+
 	@Test
-	void testRetrieveExchangeValue() {
+	void test() {
 		assertTrue(true);
 	}
 
